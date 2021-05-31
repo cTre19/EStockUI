61b98a5d7e9edfa7dc5de282d1a97210b2605b12
cTre19 <charlie.19.ct@gmail.com>
2021-05-30 20:45:42 -0500

    fixed search stock to send right request

--- src/app/search-stocks/search-stocks.component.ts
+++ src/app/search-stocks/search-stocks.component.ts
@@ -1,50 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SelectItem } from 'primeng/api';
 import { CompanyInterface } from '../models/company.interface';
+import { Company } from '../models/company.model';
 import { CompanyStock } from '../models/companystock.model';
 import { Stock } from '../models/stock.model';
 import { CompanyService } from '../services/company.service';
 import { StockService } from '../services/stock.service';
+import { DropdownModule } from 'primeng/dropdown';
+
 
 @Component({
   selector: 'app-search-stocks',
   templateUrl: './search-stocks.component.html',
   styleUrls: ['./search-stocks.component.css']
 })
-export class SearchStocksComponent {
+export class SearchStocksComponent implements OnInit {
 
-  companyStocklist: CompanyStock[] = new Array;
-  stockList: Stock[] = new Array;
-  companyCodeList: CompanyInterface[];
-  searchFlag: boolean = false;
-  selectedCompanyCode: string = "blank";
-  selectedStartDate: Date = new Date;
-  selectedEndDate: Date = new Date;
+  companyList: SelectItem[];
+  selectedCompany!: SelectItem;
+  selectedStartDate!: Date;
+  selectedEndDate!: Date;
+  stockList!: Stock[];
+  // companyStocklist: CompanyStock[] = new Array;
+  // searchFlag: boolean = false;
+  // searchStocksForm: FormGroup;
+  errorMessage: string = '';
+
 
   constructor(private stockService: StockService,
-    private companyService: CompanyService) {
-      this.companyCodeList  = [
-        { name: 'Amazon', code: 'AMZ' },
-        { name: 'Cognizant', code: 'CTSH'}
-      ];
-    }
+    private companyService: CompanyService, private dropdown: DropdownModule) {
+    this.companyList = [
+      { label: 'Amazon', value: 'AMZ' },
+      { label: 'Cognizant', value: 'CTSH' }
+    ];
+    // this.searchStocksForm = new FormGroup({
+    //   companyCode: new FormControl(''),
+    //   selectedStartDate: new FormControl(),
+    //   selectedEndDate: new FormControl()
+    // });
+  }
+
+  ngOnInit() {
+
+  }
 
   fetchCompanyNames(): void {
     this.companyService.getAllCompaniesInfo().subscribe(
       data => {
         console.log('loading company info');
-        this.companyStocklist = data;
+        // this.companyStocklist = data;
       }
     )
   }
 
+  // changeStockCode(selectedValue: any) {
+  //   this.selectedCompany = selectedValue;
+  //   console.log(this.selectedCompany);
+  // }
+
   searchStocks() {
-    this.stockService.searchStockByDateRange(this.selectedCompanyCode, 
+    console.log(this.selectedCompany);
+    console.log(this.selectedStartDate);
+    console.log(this.selectedEndDate);
+    
+    // this.errorMessage = 'All fields are required before submitting the form!';
+    this.stockService.searchStockByDateRange(this.selectedCompany.toString(),
       this.selectedStartDate.toISOString(), this.selectedEndDate.toISOString()).subscribe(
         data => {
+          console.log('inside searchStocks');
           this.stockList = data;
+        },
+        error => {
+          console.error('unable to search stocks');
         }
-      )
+      );
   }
 
 }
